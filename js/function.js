fd88fd54ba1e157de021ae7916c477198c2a538c
Krister Kaattari <krister.kaattari@edu.keuda.fi>
2024-09-12 12:54:33 +0300

    valmis

---
+++ js/function.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const noppaLuku = (min, max) => {
+    return Math.floor(Math.random() * max) + min
+}
+
+document.getElementById('noppa').addEventListener('click', () => {
+    const noppaluku = noppaLuku(1,6)
+    const kuva = document.getElementById('kuva')
+    const kuvat = '<img src="./img/' + noppaluku + '.png">'
+    kuva.innerHTML  = kuvat
+})
+
